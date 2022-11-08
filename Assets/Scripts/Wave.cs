b3b814d0bbc50561bcf3bda6bd37ce0a082324c1
jurajzalabai <juraj.zalabai@gmail.com>
2022-11-09 00:25:30 +0100

    added wave spawner v2

---
+++ Assets/Scripts/Wave.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class Wave
+{
+    public GameObject[] enemies;
+    public float[] countEnemies;
+    public float timeBetweenSpawns;
+}
